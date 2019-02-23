d2294fcce8585693175590b7659d1c87856fb69e
Paulo Cesar Loayza Carrasco <paulocesarloayza@upsa.edu.bo>
2019-02-23 13:37:09 -0400

    se agrego la función suma

---
+++ WSaritmetica/WCFaritmetica/IsrvSuma.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WCFaritmetica
+{
+    [ServiceContract]
+    public interface IsrvSuma
+    {
+
+        [OperationContract]
+        clsSuma suma(int numero1, int numero2);
+
+    }
+
+    [DataContract]
+    public class clsSuma
+    {
+        [DataMember]
+        public int numeroA { get; set; }
+        [DataMember]
+        public int numeroB { get; set; }
+        [DataMember]
+        public int resultado { get; set; }
+    }   
+}
